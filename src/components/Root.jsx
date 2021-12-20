9500bf65f89b474a623f75ab09ce3dc50e9b9bb7
mrhrifat <mrhrifat383759@gmail.com>
2021-12-20 12:59:42 +0600

    Add Root components for handle all components

---
+++ src/components/Root.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Root = () => {
+    return (
+        <div>
+            <h4>Bismillahir Rahmanir Rahim</h4>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. At nam illum voluptas atque ducimus! Aliquam, eligendi voluptatum? Cum, inventore excepturi, maiores optio odit repellendus nostrum labore aut impedit fuga ullam!
+            </p>
+        </div>
+    )
+}
+
+export default Root
