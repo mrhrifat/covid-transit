d2ab82e48d4556779cfc33abcbe714ce34901cd3
mrhrifat <mrhrifat383759@gmail.com>
2021-12-22 00:30:52 +0600

    Created State & Set data to it & send as props to another components

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -5,16 +5,41 @@ import styles from './root.module.css'
 
 class Root extends Component {
 
+    state = {
+        data: {}
+    }
+
     async componentDidMount() {
+
+        //! 1st Way        
         // const data = await fetchData()
-        const { data } = await fetchData()
-        console.log(data)
+
+        //! 2nd Way         
+        // const { data } = await fetchData()
+        // console.log(data)
+
+        //! 3rd Way
+        // const { modifiedData } = await fetchData()
+        // console.log(modifiedData)
+
+        //! 4th Way
+        // const data = await fetchData()
+        // console.log(fetchedData)
+        // this.setState({ data })
+
+        //! 5th Way
+        const fetchedData = await fetchData()
+        // console.log(fetchedData)
+        this.setState({ data: fetchedData })
     }
 
     render() {
+        // console.log(this.state.data)
+        const { data } = this.state
+        // console.log(data)
         return (
             <div className={styles.container}>
-                <Card />
+                <Card data={data} />
                 <Country />
                 <Chart />
             </div>
