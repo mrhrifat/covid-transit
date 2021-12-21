3b4ab27a460457a81f71d1c89c78c0417a834e25
mrhrifat <mrhrifat383759@gmail.com>
2021-12-22 00:22:01 +0600

    Create 5 way to fetch api & keep the best one

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,70 @@ import axios from 'axios';
 
 export const fetchData = async () => {
     try {
-        const response = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
-        return response
+
+        //! 1st Way
+        // const response = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+        // return response        
+        
+        
+        //! 2nd Way        
+        // const {
+        //     data
+        // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+        // return data
+        
+        
+        //! 3rd Way
+        // const {
+        //     data
+        // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+
+        // const modifiedData = {
+        //     confirmed: data.confirmed,
+        //     recovered: data.recovered,
+        //     deaths: data.deaths,
+        //     countries: data.countries
+        // }
+        // return modifiedData
+        
+        
+        //! 4th Way
+        // const {
+        //     data: {
+        //         confirmed,
+        //         recovered,
+        //         deaths,
+        //         countries
+        //     }
+        // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+
+        // const modifiedData = {
+        //     confirmed,
+        //     recovered,
+        //     deaths,
+        //     countries,
+        // }
+        // return modifiedData
+
+        
+        
+        //! 5th Way
+        const {
+            data: {
+                confirmed,
+                recovered,
+                deaths,
+                countries
+            }
+        } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+
+        return {
+            confirmed,
+            recovered,
+            deaths,
+            countries,
+        }
+
 
     } catch (err) {
         console.log(err)
