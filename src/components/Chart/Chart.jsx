d8a66f909f4c16648dc51b9e56adc7d1fc4ed2ff
mrhrifat <mrhrifat383759@gmail.com>
2021-12-22 04:47:46 +0600

    Fetch Data from API

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { fetchDailyData } from '../../api'
+
 
 const Chart = () => {
+
+    const [dailyData, setDailyData] = useState({})
+
+    useEffect(() => {
+        const dailyData = async () => {
+            // const getDailyData = await fetchDailyData()
+            setDailyData(await fetchDailyData())
+        }
+        dailyData()
+    })
+
+    // console.log(dailyData)
+
     return (
+
         <div>
             Chart
         </div>
