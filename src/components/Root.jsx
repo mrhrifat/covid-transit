108587e55381337f7f6e48e802d16117f3304199
mrhrifat <mrhrifat383759@gmail.com>
2021-12-21 21:55:28 +0600

    Fetching Data Successfully

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import { Card, Chart, Country } from './'
+import { fetchData } from '../api'
 import styles from './root.module.css'
 
 class Root extends Component {
+
+    async componentDidMount() {
+        // const data = await fetchData()
+        const { data } = await fetchData()
+        console.log(data)
+    }
+
     render() {
         return (
             <div className={styles.container}>
