0f35eb11c18cff42065a7e083c3f1c9a00d50025
mrhrifat <mrhrifat383759@gmail.com>
2021-12-23 04:10:09 +0600

    Add Bar CHart JSX & Data

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -5,29 +5,21 @@ import { fetchDailyData } from '../../api'
 import classNames from 'classnames'
 import styles from './chart.module.css'
 
+
+
+// Bar & Line Chart
 import {
     Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+    registerables
 } from 'chart.js'
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+    ...registerables
 )
 
 
-const Chart = () => {
+
+const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     // console.log(props)
 
     const [dailyData, setDailyData] = useState([])
@@ -73,10 +65,36 @@ const Chart = () => {
         // <LinearProgress />
     )
 
+    // console.log(confirmed, recovered, deaths)
+
+    const barChart = (
+        confirmed
+            ? (
+                <Bar
+                    data={{
+                        labels: ['Infected', 'Recovered', 'Deaths'],
+                        datasets: [{
+                            label: 'People',
+                            backgroundColor: [
+                                'rgba(0, 0, 255, 0.5)',
+                                'rgba(0, 255, 0, 0.5)',
+                                'rgba(255, 0, 0, 0.5)'
+                            ],
+                            data: [confirmed.value, recovered.value, deaths.value]
+                        }]
+                    }}
+                    options={{
+                        legend: { dispplay: false },
+                        title: { display: true, text: `Current state is ${country}` },
+                    }}
+                />
+            )
+            : null
+    )
 
     return (
         <div className={styles.container}>
-            {lineChart}
+            {country ? barChart : lineChart}
         </div>
     )
 }
