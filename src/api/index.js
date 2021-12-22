399b43276f9304383a72ef4822be03aae42de277
mrhrifat <mrhrifat383759@gmail.com>
2021-12-23 03:36:21 +0600

    Make new way to work if pass country parameter to fetchData

--- src/api/index.js
+++ src/api/index.js
@@ -1,55 +1,90 @@
 import axios from 'axios';
 
-export const fetchData = async () => {
+// export const fetchData = async () => {
+//     try {
+
+//         // 1st Way
+//         // const response = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+//         // return response        
+
+
+//         // 2nd Way        
+//         // const {
+//         //     data
+//         // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+//         // return data
+
+
+//         // 3rd Way
+//         // const {
+//         //     data
+//         // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+
+//         // const modifiedData = {
+//         //     confirmed: data.confirmed,
+//         //     recovered: data.recovered,
+//         //     deaths: data.deaths,
+//         //     countries: data.countries
+//         // }
+//         // return modifiedData
+
+
+//         // 4th Way
+//         // const {
+//         //     data: {
+//         //         confirmed,
+//         //         recovered,
+//         //         deaths,
+//         //         countries
+//         //     }
+//         // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+
+//         // const modifiedData = {
+//         //     confirmed,
+//         //     recovered,
+//         //     deaths,
+//         //     countries,
+//         // }
+//         // return modifiedData
+
+
+
+//         // 5th Way
+//         const {
+//             data: {
+//                 confirmed,
+//                 recovered,
+//                 deaths,
+//                 lastUpdate
+//             }
+//         } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+
+//         return {
+//             confirmed,
+//             recovered,
+//             deaths,
+//             lastUpdate,
+//         }
+//         //? Getting Generated 4 Data Only
+
+
+//     } catch (err) {
+//         console.log(err)
+//         throw new Error('Fetching API Failed')
+//     }
+// }
+
+
+export const fetchData = async (country) => {
+
+    let changeURL = process.env.REACT_APP_COVID_API_BASE_URL
+
+        if (country) {
+            changeURL = `${process.env.REACT_APP_COVID_API_BASE_URL}/countries/${country}`
+        }
+
     try {
-
-        // 1st Way
-        // const response = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
-        // return response        
-
-
-        // 2nd Way        
-        // const {
-        //     data
-        // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
-        // return data
-
-
-        // 3rd Way
-        // const {
-        //     data
-        // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
-
-        // const modifiedData = {
-        //     confirmed: data.confirmed,
-        //     recovered: data.recovered,
-        //     deaths: data.deaths,
-        //     countries: data.countries
-        // }
-        // return modifiedData
-
-
-        // 4th Way
-        // const {
-        //     data: {
-        //         confirmed,
-        //         recovered,
-        //         deaths,
-        //         countries
-        //     }
-        // } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
-
-        // const modifiedData = {
-        //     confirmed,
-        //     recovered,
-        //     deaths,
-        //     countries,
-        // }
-        // return modifiedData
-
-
-
-        // 5th Way
+        
         const {
             data: {
                 confirmed,
@@ -57,7 +92,7 @@ export const fetchData = async () => {
                 deaths,
                 lastUpdate
             }
-        } = await axios.get(process.env.REACT_APP_COVID_API_BASE_URL)
+        } = await axios.get(changeURL)
 
         return {
             confirmed,
